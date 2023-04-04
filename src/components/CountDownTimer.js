4c98a6062cc54abf7a44d3fee8752dba9f74beda
gundlurubalaji <balajirock7093@gmail.com>
2023-04-04 15:51:53 +0530

    balji

---
+++ src/components/CountDownTimer.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import TimerInput from "./TimerInput"
+
+
+function CountDownTimer() {
+
+    const [currentTime, setCurrentTime] = useState(0)
+    const [intervalId, setIntervalId] = useState(null)
+
+    const startNewTimer = () => {
+        setIntervalId(setInterval(() => {
+            setCurrentTime(currentTime => {
+                if (currentTime > 0) {
+                    return currentTime - 1
+                }
+                destroyExistingTimer()
+                return currentTime
+            })
+        }, 1000))
+    }
+
+    const destroyExistingTimer = () => {
+        if (intervalId) {
+            clearInterval(intervalId)
+            setIntervalId(null)
+        }
+    }
+
+    const startTimer = (timeInSeconds) => {
+        console.log(`Starting timer with time ${timeInSeconds} seconds`)
+        destroyExistingTimer()
+        setCurrentTime(timeInSeconds)
+        startNewTimer()
+
+    }
+
+    return <div id="count-down-app">
+        <TimerInput onRequestStart={startTimer} />
+        <div id="current-time">
+            {currentTime}
+        </div>
+    </div>
+}
+
+export default CountDownTimer
